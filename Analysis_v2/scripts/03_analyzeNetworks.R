004236616a53d5123d1bc39ede411070bc25bb27
Siep <s.van.amsterdam@ncmm.uio.no>
2025-03-11 17:11:53 +0100

    Added gsea logic to analysis script Restructured code into functions Plot correlation matrix, linear regression and top pathways from gsea

--- Analysis_v2/scripts/03_analyzeNetworks.R
+++ Analysis_v2/scripts/03_analyzeNetworks.R
@@ -11,9 +11,12 @@ if (!requireNamespace("fgsea", quietly = TRUE)) BiocManager::install("fgsea")
 
 # Load required packages
 library(ggplot2)
+library(patchwork)
 library(msigdbr)
 library(fgsea)
 library(Matrix)
+library(dplyr)
+library(biomaRt)
 
 # Set working directory
 setwd("/div/pythagoras/u1/siepv/siep/Analysis_v2/output/networks")
@@ -26,52 +29,57 @@ load("final/kidneyScorpionOutput.Rdata")
 load("final/liverScorpionOutput.Rdata")
 
 
-#### 2. Calculate indegrees & outdegrees ####       #rewrite to function!!
-blood_indegrees <- data.frame(gene = colnames(blood_output[[1]]$regNet))
-for (celltype in names(blood_output)) {
-    regnet <- blood_output[[celltype]]$regNet
-    blood_indegrees[[paste0("blood_", celltype)]] <- colSums(regnet)
-}
-lung_indegrees <- data.frame(gene = colnames(lung_output[[1]]$regNet))
-for (celltype in names(lung_output)) {
-    regnet <- lung_output[[celltype]]$regNet
-    lung_indegrees[[paste0("lung_", celltype)]] <- colSums(regnet)
-}
-fat_indegrees <- data.frame(gene = colnames(fat_output[[1]]$regNet))
-for (celltype in names(fat_output)) {
-    regnet <- fat_output[[celltype]]$regNet
-    fat_indegrees[[paste0("fat_", celltype)]] <- colSums(regnet)
-}
-kidney_indegrees <- data.frame(gene = colnames(kidney_output[[1]]$regNet))
-for (celltype in names(kidney_output)) {
-    regnet <- kidney_output[[celltype]]$regNet
-    kidney_indegrees[[paste0("kidney_", celltype)]] <- colSums(regnet)
-}
-liver_indegrees <- data.frame(gene = colnames(liver_output[[1]]$regNet))
-for (celltype in names(liver_output)) {
-    regnet <- liver_output[[celltype]]$regNet
-    liver_indegrees[[paste0("liver_", celltype)]] <- colSums(regnet)
+#### 2. Calculate indegrees & outdegrees ####
+
+calculate_indegrees <- function(output, tissue_name) {
+    indegrees <- data.frame(gene = colnames(output[[1]]$regNet))
+    for (celltype in names(output)) {
+        regnet <- output[[celltype]]$regNet
+        indegrees[[paste0(tissue_name, "_", celltype)]] <- colSums(regnet)
+    }
+    return(indegrees)
 }
 
+blood_indegrees <- calculate_indegrees(blood_output, "blood")
+lung_indegrees <- calculate_indegrees(lung_output, "lung")
+fat_indegrees <- calculate_indegrees(fat_output, "fat")
+kidney_indegrees <- calculate_indegrees(kidney_output, "kidney")
+liver_indegrees <- calculate_indegrees(liver_output, "liver")
+
 # Combine blood and lung indegrees and outdegrees into one dataframe
-combined_indegrees <- Reduce(function(x, y) merge(x, y, by = "gene", all = TRUE), list(blood_indegrees, lung_indegrees, fat_indegrees, kidney_indegrees, liver_indegrees))
+combined_indegrees <- Reduce(function(x, y) merge(x, y, by = "gene", all = TRUE), list(blood_indegrees, lung_indegrees)) # , fat_indegrees, kidney_indegrees, liver_indegrees))
 # combined_indegrees <- na.omit(combined_indegrees)
 
 # Calculate correlation matrix
 cor_indegree_matrix <- cor(combined_indegrees[, -1])
 
+# Convert the correlation matrix to a long format for ggplot
+cor_long <- as.data.frame(as.table(cor_indegree_matrix))
+
+setwd("/div/pythagoras/u1/siepv/siep/Analysis_v2/output/analysis")
+
+# Plot the one-sided correlation matrix with values in the tiles
+ggsave("correlation_matrix_indegrees.pdf", ggplot(cor_long, aes(Var1, Var2, fill = Freq)) +
+    geom_tile(na.rm = TRUE) +
+    geom_text(aes(label = round(Freq, 3)), color = "black", size = 3, na.rm = TRUE) +
+    scale_fill_gradient(low = "white", high = "steelblue", name = "Pearson's r value") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
+    labs(x = "Cell Types", y = "Cell Types", title = "Correlation Matrix of Indegrees"),
+width = 12, height = 6
+)
 
 #### 3. Calculate linear regression and residuals between conditions ####
 
 # Define comparisons
 comparisons <- list(
-    list(name = "blood_platelet_vs_lung_pneumo", x = "blood_platelet", y = "lung_type_ii_pneumocyte"),
-    list(name = "blood_cd4_vs_cd8", x = "blood_cd4_positive_t_cell", y = "blood_cd8_positive_t_cell"),
-    list(name = "blood_cd4_vs_monocyte", x = "blood_cd4_positive_t_cell", y = "blood_monocyte"),
+    list(name = "cd4_blood_vs_lung", x = "blood_cd4_positive_t_cell", y = "lung_cd4_positive_t_cell"),
+    list(name = "cd8_blood_vs_lung", x = "blood_cd8_positive_t_cell", y = "lung_cd8_positive_t_cell"),
+    list(name = "monocyte_blood_vs_lung", x = "blood_monocyte", y = "lung_monocyte"),
     list(name = "lung_cd4_vs_cd8", x = "lung_cd4_positive_t_cell", y = "lung_cd8_positive_t_cell"),
     list(name = "lung_cd4_vs_monocyte", x = "lung_cd4_positive_t_cell", y = "lung_monocyte"),
-    list(name = "cd4_blood_vs_lung", x = "blood_cd4_positive_t_cell", y = "lung_cd4_positive_t_cell"),
-    list(name = "cd8_blood_vs_lung", x = "blood_cd8_positive_t_cell", y = "lung_cd8_positive_t_cell")
+    list(name = "lung_cd8_vs_monocyte", x = "lung_cd8_positive_t_cell", y = "lung_monocyte"),
+    list(name = "blood_platelet_vs_lung_2pneumo", x = "blood_platelet", y = "lung_type_ii_pneumocyte")
 )
 
 # Function to fit linear model and extract + rank residuals descending
@@ -79,37 +87,63 @@ calculate_residuals <- function(data, x_con, y_con) {
     fit <- lm(data[[y_con]] ~ data[[x_con]], data = data) # Fit linear model
     res <- residuals(fit)
     names(res) <- data$gene
-    return(res[order(-res)]) # Rank residuals
+    return(list(fit = fit, residuals = res[order(-res)])) # Return fit and ranked residuals
 }
 
+# Function to plot linear regression with ggplot
+plot_linear_regression <- function(data, x_con, y_con, comp_name) {
+    ggplot(data, aes(x = x_con, y = y_con)) +
+        geom_point(alpha = 0.5) +
+        geom_smooth(method = "lm", se = FALSE, color = "red") +
+        labs(
+            title = paste("Linear Regression:", comp_name),
+            x = x_con,
+            y = y_con
+        ) +
+        theme_minimal()
+}
+
+# Calculate residuals for each comparison
 residuals_list <- list()
+fit_list <- list()
+plot_list <- list()
 for (comp in comparisons) {
     cat("Calculating residuals for:", comp$name, "\n")
-    residuals_list[[comp$name]] <- calculate_residuals(combined_indegrees, comp$x, comp$y)
+    result <- calculate_residuals(combined_indegrees, comp$x, comp$y)
+    residuals_list[[comp$name]] <- result$residuals
+    fit_list[[comp$name]] <- result$fit
+    plot_list[[comp$name]] <- plot_linear_regression(combined_indegrees, comp$x, comp$y, comp$name)
 }
 
+# Save plots of the linear regression models
+ggsave("immune_cells_between_tissues.pdf", patchwork::wrap_plots(A = plot_list[[1]], B = plot_list[[2]], C = plot_list[[3]], design = "AABB\n#CC#"), width = 12, height = 6)
+ggsave("immune_cells_lung.pdf", patchwork::wrap_plots(A = plot_list[[4]], B = plot_list[[5]], C = plot_list[[6]], design = "AABB\n#CC#"), width = 12, height = 6)
+ggsave("tissue_specific_linear.pdf", plot_list[[7]], width = 12, height = 6)
 
-#### 4. Run GSEA on residuals ####
+
+
+#### 4. Run GSEA on ranked residuals ####
 
 # Load gene sets for GO:BP and GO:MF using gene symbols
 msigdb_BP <- msigdbr(species = "Homo sapiens", category = "C5", subcategory = "BP")[, c("gs_name", "gene_symbol")]
 msigdb_MF <- msigdbr(species = "Homo sapiens", category = "C5", subcategory = "MF")[, c("gs_name", "gene_symbol")]
 
 # Convert to fgsea-compatible format (list of gene sets)
-msigdb_BP_list <- split(msigdb_BP$gene_symbol, msigdb_BP$gs_name)
-msigdb_MF_list <- split(msigdb_MF$gene_symbol, msigdb_MF$gs_name)
+msigdb_BP <- split(msigdb_BP$gene_symbol, msigdb_BP$gs_name)
+msigdb_MF <- split(msigdb_MF$gene_symbol, msigdb_MF$gs_name)
 
 
 gsea_results <- list()
 
-for (comp in names(residuals_list[1:2])) {
+options(warn = 1)
+for (comp in names(residuals_list)) {
     cat("Running GSEA for:", comp, "\n")
 
     ranked_genes <- residuals_list[[comp]] # Get ranked gene list
 
     # Run fgsea for GO:BP
-    gsea_results[[paste0(comp, "_BP")]] <- fgsea(
-        pathways = msigdb_BP_list,
+    gsea_results[[paste0("BP_", comp)]] <- fgsea(
+        pathways = msigdb_BP,
         stats = ranked_genes,
         minSize = 10,
         maxSize = 500,
@@ -117,14 +151,93 @@ for (comp in names(residuals_list[1:2])) {
     )
 
     # Run fgsea for GO:MF
-    gsea_results[[paste0(comp, "_MF")]] <- fgsea(
-        pathways = msigdb_MF_list,
+    gsea_results[[paste0("MF_", comp)]] <- fgsea(
+        pathways = msigdb_MF,
         stats = ranked_genes,
         minSize = 10,
         maxSize = 500,
-        nperm = 10000
+        nproc = 1
     )
 }
+
+
+# Function to plot top pathways
+plot_top_pathways <- function(gsea_result, comparison_name) {
+    sig_pathways <- gsea_result %>%
+        filter(padj < 0.05)
+
+    topPathwaysUp <- head(sig_pathways[NES > 0], 10) %>%
+        arrange(NES)
+    topPathwaysUp$pathway <- factor(topPathwaysUp$pathway, levels = topPathwaysUp$pathway)
+
+    topPathwaysDown <- head(sig_pathways[NES < 0], 10) %>%
+        arrange(NES)
+    topPathwaysDown$pathway <- factor(topPathwaysDown$pathway, levels = topPathwaysDown$pathway)
+
+    topPathways <- bind_rows(topPathwaysUp, topPathwaysDown) %>%
+        arrange(NES)
+    topPathways$pathway <- factor(topPathways$pathway, levels = topPathways$pathway)
+
+    p1 <- ggplot(topPathwaysUp, aes(x = NES, y = pathway, size = size, fill = NES)) +
+        geom_point(shape = 21) +
+        scale_size_area(max_size = 10) +
+        scale_fill_gradient(low = "white", high = "red") +
+        theme(axis.text.y = element_text(size = 11), axis.title.y = element_blank()) +
+        xlim(min(topPathwaysUp$NES), max(topPathwaysUp$NES)) +
+        xlab("NES") +
+        ggtitle(paste("Top 10 enriched pathways in", comparison_name, "upregulated"))
+
+    p2 <- ggplot(topPathwaysDown, aes(x = NES, y = pathway, size = size, fill = NES)) +
+        geom_point(shape = 21) +
+        scale_size_area(max_size = 10) +
+        scale_fill_gradient(low = "blue", high = "white") +
+        theme(axis.text.y = element_text(size = 11), axis.title.y = element_blank()) +
+        xlim(min(topPathwaysDown$NES), max(topPathwaysDown$NES)) +
+        xlab("NES") +
+        ggtitle(paste("Top 10 enriched pathways in", comparison_name, "downregulated"))
+
+    return(list(up = p1, down = p2))
+}
+
+# Generate plots for all comparisons in gsea_results and save to a list
+gsea_plots <- list()
+for (comp in names(gsea_results)) {
+    cat("Plotting GSEA results for:", comp, "\n")
+    gsea_plots[[comp]] <- plot_top_pathways(gsea_results[[comp]], comp)
+}
+
+dotplot <- gsea_plots[[7]]$up
+print(dotplot)
+filename <- paste0(names(gsea_plots)[7], "_gsea_plot.pdf")
+ggsave(filename, dotplot, width = 12, height = 8)
+
+
+
+# pdf(file = "7_gsea_top20pathways.pdf", width = 20, height = 15)
+# plotGseaTable(msigdb_BP[topPathways], stats = ranked_genes, fgseaRes = sig_pathways, gseaParam = 0.5)
+# dev.off()
+
+
+# plotEnrichment(
+#     msigdb_BP_list[[gsea_results[[1]][order(padj), ][1]$pathway]],
+#     ranked_genes
+# ) +
+#     labs(title = gsea_results[[1]][order(padj), ][1]$pathway)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -165,8 +278,8 @@ plotEnrichment(
 
 
 
-top_genes <- names(residuals_list$blood_platelet_vs_lung_pneumo)[1:1000] # Take the top 500 most deviated genes
-neg_genes <- names(rev(residuals_list$blood_platelet_vs_lung_pneumo))[1:1000]
+top_genes <- names(residuals_list[[7]])[1:1000] # Take the top 1000 most deviated genes
+neg_genes <- names(rev(residuals_list[[7]]))[1:1000]
 
 missing_top_genes <- setdiff(top_genes, msigdb_BP$gene_symbol)
 missing_neg_genes <- setdiff(neg_genes, msigdb_BP$gene_symbol)
@@ -189,7 +302,7 @@ ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 gene_annotations <- getBM(
     attributes = c("external_gene_name", "gene_biotype"),
     filters = "external_gene_name",
-    values = missing_top_genes, # or missing_neg_genes
+    values = names(head(residuals_list[[1]], 1000)), # or missing_neg_genes
     mart = ensembl
 )
 
